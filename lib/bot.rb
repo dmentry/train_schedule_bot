df1b7cd40a4b90fcbbca0af0bd7b8f76debbc96a
dack99 <slipin2020@mail.ru>
2023-06-21 23:54:39 +0300

    Fix bug

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,6 +3,7 @@ class Bot
     @max_lines    = max_lines
     @bot_token    = bot_token
     @url          = url
+    @out  = []
 
     clear_values
   end
@@ -17,88 +18,94 @@ class Bot
         bot.listen do |message|
           next if start_bot_time - message.date > 650
 
-          if message.text == '/start'
-            clear_values
+          if !message&.text.nil?
+            if message.text == '/start'
+              clear_values
 
-            bot.api.send_Message(chat_id: message.chat.id, text: "Привет, #{ message.from.first_name }!")
+              bot.api.send_Message(chat_id: message.chat.id, text: "Привет, #{ message.from.first_name }!")
 
-            send_msg_with_keabord(bot: bot, message: message, question: 'Выберите, когда ехать:', keyboard_values: [['Сегодня', 'Завтра']])
-          elsif message.text == '/stop'
-            bye_message(bot: bot, message: message)
+              send_msg_with_keabord(bot: bot, message: message, question: 'Выберите, когда ехать:', keyboard_values: [['Сегодня', 'Завтра']])
+            elsif message.text == '/stop'
+              bye_message(bot: bot, message: message)
 
-            clear_values
-          elsif STATIONS_LIST1.include?(message.text.strip.downcase) || STATIONS_LIST2.include?(message.text.strip.downcase)
-            station_name = if STATIONS_LIST2.include?(message.text.strip.downcase)
-                             'Москва Курская'
-                           else
-                             message.text.strip
-                           end
-            if @from.empty?
-              @from = station_name
-            else
-              @to = station_name
-            end
+              clear_values
+            elsif STATIONS_LIST1.include?(message.text.strip.downcase) || STATIONS_LIST2.include?(message.text.strip.downcase)
+              station_name = if STATIONS_LIST2.include?(message.text.strip.downcase)
+                               'Москва Курская'
+                             else
+                               message.text.strip
+                             end
+              if @from.empty?
+                @from = station_name
+              else
+                @to = station_name
+              end
 
-            if @to.empty?
-              send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию прибытия из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
-            end
+              if @to.empty?
+                send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию прибытия из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
+              end
 
-            if !@from.empty? && !@to.empty?
-              @out = parsing.schedule(from: @from, to: @to, check_date: @check_date, max_lines: MAX_LINES)
+              if !@from.empty? && !@to.empty?
+                @out = parsing.schedule(from: @from, to: @to, check_date: @check_date, max_lines: MAX_LINES)
 
-              @quantity_of_schedule_packs = @out.size - 1
+                @quantity_of_schedule_packs = @out.size - 1
 
-              @out[@schedule_pack_index].each do |schedule_line|
-                bot.api.send_message(chat_id: message.chat.id, text: schedule_line, parse_mode: 'HTML')
+                @out[@schedule_pack_index].each do |schedule_line|
+                  bot.api.send_message(chat_id: message.chat.id, text: schedule_line, parse_mode: 'HTML')
 
-                if schedule_line == "Электричек на указанную дату нет."
-                  clear_values
-                  # Если не приравнять к нулю, то дальше из-за условия выскочит вопрос 'Еще?'
-                  @quantity_of_schedule_packs = 0
+                  if schedule_line == "Электричек на указанную дату нет."
+                    clear_values
+                    # Если не приравнять к нулю, то дальше из-за условия выскочит вопрос 'Еще?'
+                    @quantity_of_schedule_packs = 0
+                  end
+                end
+
+                @schedule_pack_index += 1
+
+                if @schedule_pack_index <= @quantity_of_schedule_packs 
+                  send_msg_with_keabord(bot: bot, message: message, question: 'Еще?', keyboard_values: [['Да', 'Нет']])
+                else
+                  bye_message(bot: bot, message: message)
                 end
               end
+            elsif message.text == 'Сегодня'
+              @check_date = Date.today
 
-              @schedule_pack_index += 1
+              send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию отправления из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
+            elsif message.text == 'Завтра'
+              @check_date = (Date.today + 1)
 
-              if @schedule_pack_index <= @quantity_of_schedule_packs 
-                send_msg_with_keabord(bot: bot, message: message, question: 'Еще?', keyboard_values: [['Да', 'Нет']])
+              send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию отправления из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
+            elsif message.text == 'Да'
+              if !@out.empty?
+                @out[@schedule_pack_index].each do |schedule_line|
+                  bot.api.send_message(chat_id: message.chat.id, text: schedule_line, parse_mode: 'HTML')
+                end
+
+                @schedule_pack_index += 1
+
+                if @schedule_pack_index <= @quantity_of_schedule_packs 
+                  send_msg_with_keabord(bot: bot, message: message, question: 'Еще?', keyboard_values: [['Да', 'Нет']])
+                else
+                  bye_message(bot: bot, message: message)
+                end
               else
-                bye_message(bot: bot, message: message)
+                bot.api.send_message(chat_id: message.chat.id, text: "Сначала начните диалог, нажав на '/start'")
               end
-            end
-          elsif message.text == 'Сегодня'
-            @check_date = Date.today
+            elsif message.text == 'Нет'
+              bye_message(bot: bot, message: message)
 
-            send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию отправления из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
-          elsif message.text == 'Завтра'
-            @check_date = (Date.today + 1)
-
-            send_msg_with_keabord(bot: bot, message: message, question: 'Выберите станцию отправления из списка:', keyboard_values: [BUTTONS_ARR1, BUTTONS_ARR2])
-          elsif message.text == 'Да'
-            if !@out.empty?
-              @out[@schedule_pack_index].each do |schedule_line|
-                bot.api.send_message(chat_id: message.chat.id, text: schedule_line, parse_mode: 'HTML')
-              end
-
-              @schedule_pack_index += 1
-
-              if @schedule_pack_index <= @quantity_of_schedule_packs 
-                send_msg_with_keabord(bot: bot, message: message, question: 'Еще?', keyboard_values: [['Да', 'Нет']])
-              else
-                bye_message(bot: bot, message: message)
-              end
+              clear_values
             else
-              bot.api.send_message(chat_id: message.chat.id, text: "Сначала начните диалог, нажав на '/start'")
+              bot.api.send_message(chat_id: message.chat.id, text: "Неизвестная команда.")
+              bye_message(bot: bot, message: message, additional_text: "Попробуйте начать заново, нажав /start.\n")
+
+              clear_values
             end
-          elsif message.text == 'Нет'
-            bye_message(bot: bot, message: message)
-
-            clear_values
           else
-            bot.api.send_message(chat_id: message.chat.id, text: "Неизвестная команда.")
-            bye_message(bot: bot, message: message, additional_text: "Попробуйте начать заново, нажав /start.\n")
-
             clear_values
+
+            bye_message(bot: bot, message: message, additional_text: "Попробуйте начать заново, нажав /start.\n")
           end
         end
       end
